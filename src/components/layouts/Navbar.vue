e56b8450c43b8d5ea31612c39bb898e6809e5d65
jperic <juraj.peric@barrage.net>
2023-03-20 12:31:37 +0100

    project init, added home and navbar

---
+++ src/components/layouts/Navbar.vue
@@ -0,0 +1,69 @@
+<template>
+    <nav class="nav">
+        <div class="nav_inner">
+            <div class="nav_inner-left">
+                <RouterLink :to="{ name: 'home' }">
+                    <img class="pokeball-logo" src="../../assets/images/logo.png" alt="pokeball">
+                </RouterLink>
+            </div>
+            <div class="nav_inner-right">
+                <RouterLink to="/">
+                    <button class="pokedex-btn">
+                        Pokedex
+                    </button>
+                </RouterLink>
+            </div>
+        </div>
+
+    </nav>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+.nav {
+    background-color: var(--secondary-color);
+    opacity: 0.85;
+    padding: 10px 50px;
+    position: fixed;
+    top: 10px;
+    left: 10px;
+    right: 10px;
+    z-index: 1000;
+    border-radius: 25px;
+}
+
+.nav_inner {
+    height: 65px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.nav_inner-left {
+    height: 100%;
+}
+
+.nav_inner-right {
+    height: 100%;
+    display: flex;
+    align-items: center;
+}
+
+.pokeball-logo {
+    height: 100%;
+}
+
+.pokedex-btn {
+    color: var(--secondary-color);
+    text-align: center;
+    background-color: var(--primary-color);
+    border: 0px;
+    font-size: 1.3em;
+    padding: 12px 25px;
+    border-radius: 12px;
+    cursor: pointer;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="home-wrapper">
+        <div class="home-inner">
+            <div class="home-content">
+                <div class="title-container">
+                    <img class="pokemon-title" src="../assets/images/pokemon-text.png" alt="Pokemon">
+                </div>
+                <Router-link to="/play" class="ball__link">
+                    <div class="ball">
+                        <div class="ball__button"></div>
+                        <div class="ball_text">
+                            <img class="ball_text_img" src="src/assets/images/pngaaa.com-1906169.png" alt="GUESS THAT POKEMON">
+                        </div>
+                    </div>
+                </Router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.home-wrapper {
+    background-repeat: no-repeat;
+    background-image: url('../assets/images/Pokémon-wallpapers-5.jpg');
+    background-size: cover;
+    background-position: 50%;
+    height: 100vh;
+    width: 100%;
+}
+
+.home-inner {
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100%;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.home-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.pokemon-title {
+    width: 500px;
+}
+
+.ball {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background-image: -webkit-radial-gradient(60% 40%, circle, rgba(0, 0, 0, 0) 30%, rgba(0, 0, 0, 1) 100%),
+        -webkit-linear-gradient(-90deg, #D00 45%, #333 45%, #333 55%, #FFF 55%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.ball__button {
+    height: 30px;
+    width: 30px;
+    background-color: #FFF;
+    border-radius: 100%;
+    border-color: #333;
+    border-style: solid;
+    border-width: 2px;
+    box-shadow: 0 0 0 5px #fff, 0 0 0 12px #333;
+}
+
+.ball_text {
+    position: absolute;
+    visibility: hidden;
+    height: 0%;
+}
+.ball_text_img{
+    height: 100%;
+}
+.ball:hover{
+    animation: shake .3s;
+    animation-iteration-count: infinite;
+}
+.ball:hover > .ball_text {
+    visibility: visible;
+    transition: height 0.4s ease-in;
+    height: 75%;
+}
+@keyframes shake {
+    0%, 100% {
+        rotate: -10deg;
+    }
+    50% {
+        rotate: 10deg;
+    }
+}
+</style>
