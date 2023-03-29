6e97e3242a4a1ace9516261baad87da1bba020a7
jperic <juraj.peric@gmail.com>
2023-03-29 10:56:12 +0200

    store changed

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,25 +1,41 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import axios from 'axios'
+
 
 type PokemonState = {
-  randomPokemonId?: number,
-  randomPokemonImg?: string,
-  maxPokemons: number
+  maxPokemons: number,
+  pokemonApiUrl: string
+  pokemonPictureUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork',
+  pokedex: Set<number>
 }
 
 export const usePokemonStore = defineStore('pokemon', {
   state: ():PokemonState => ({
-    randomPokemonId: undefined,
-    randomPokemonImg: undefined,
-    maxPokemons: 151
+    maxPokemons: 151,
+    pokemonApiUrl: ' https://pokeapi.co/api/v2/pokemon/',
+    pokemonPictureUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork',
+    pokedex: new Set<number>()
   }),
   actions: {
-    getRandomPokemonIdAndPic() {
-      const id = Math.floor(Math.random() * this.maxPokemons) + 1
-      const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
-      this.randomPokemonId = id
-      this.randomPokemonImg = img
+    getRandomPokemonId() {
+      let id:number
+      do{
+        id = Math.floor(Math.random() * this.maxPokemons) + 1
+      }while(this.pokedex.has(id))
+      return id
+    },
+    async getPokemonName(id: number) {
+      try{
+        const request = await fetch(this.pokemonApiUrl + id)
+        const response = await request.json()
+        const pokemon = response.forms[0].name
+        return pokemon
+      }catch{
+        
+      }
+    },
+    storeToPokedex(id:number) {
+      this.pokedex.add(id)
     }
   }
 })
