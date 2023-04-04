4442966a7129d7c01d442bcc7fc970aa7d66e7c2
jperic <juraj.peric@gmail.com>
2023-04-04 11:07:12 +0200

    pokemon component

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,35 @@
+<script setup lang='ts'>
+const props = defineProps({
+    pokemonImg: {
+        type: String,
+        default: '',
+    },
+    display: {
+        type: Boolean,
+        default: false,
+    },
+})
+</script>
+
+<template>
+    <div class="poke-img-holder"
+        :style="{ '-webkit-mask-image': `url(${props.pokemonImg})`, 'mask-image': `url(${props.pokemonImg})` }">
+        <img :src="props.pokemonImg" alt="Pokemon" v-if="display">
+    </div>
+</template>
+
+<style scoped>
+.poke-img-holder {
+    height: 70%;
+    width: 70%;
+    background-color: black;
+    -webkit-mask-position: center;
+    -webkit-mask-repeat: no-repeat;
+    -webkit-mask-size: contain;
+    mask-position: center;
+    mask-repeat: no-repeat;
+    mask-size: contain;
+    display: flex;
+    justify-content: center;
+}
+</style>
