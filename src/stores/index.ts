1856eeadf720424617473f86df1c98aca0cba3ce
jperic <juraj.peric@gmail.com>
2023-04-04 11:13:30 +0200

    store changes updated

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -4,38 +4,73 @@ import { defineStore } from 'pinia'
 
 type PokemonState = {
   maxPokemons: number,
+  totalPages: number,
+  isLoading: boolean,
   pokemonApiUrl: string
-  pokemonPictureUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork',
+  pokemonPictureUrl: string,
   pokedex: Set<number>
 }
 
 export const usePokemonStore = defineStore('pokemon', {
-  state: ():PokemonState => ({
+  state: (): PokemonState => ({
     maxPokemons: 151,
-    pokemonApiUrl: ' https://pokeapi.co/api/v2/pokemon/',
-    pokemonPictureUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork',
+    totalPages: Math.floor(151 / 20) + 1,
+    isLoading: false,
+    pokemonApiUrl: 'https://pokeapi.co/api/v2/pokemon/',
+    pokemonPictureUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/',
     pokedex: new Set<number>()
   }),
   actions: {
     getRandomPokemonId() {
-      let id:number
-      do{
+      let id: number
+      do {
         id = Math.floor(Math.random() * this.maxPokemons) + 1
-      }while(this.pokedex.has(id))
+      } while (this.pokedex.has(id))
       return id
     },
     async getPokemonName(id: number) {
-      try{
+      try {
+        this.isLoading = true
         const request = await fetch(this.pokemonApiUrl + id)
         const response = await request.json()
         const pokemon = response.forms[0].name
+        this.isLoading = false
         return pokemon
-      }catch{
-        
+      } catch {
+
       }
     },
-    storeToPokedex(id:number) {
+    storeToPokedex(id: number) {
       this.pokedex.add(id)
+    },
+    async getPokemons(page: number) {
+      this.isLoading = true
+      let pokemons = []
+      let end
+      let start
+      if (page == this.totalPages) {
+        end = this.maxPokemons
+        start = end - this.maxPokemons % 20 + 1
+        console.log(start);
+
+      }
+      else {
+        end = page * 20
+        start = end - 19
+      }
+      for (let i = start; i <= end; i++) {
+        let name = "???"
+        if (this.pokedex.has(i)) {
+          name = await this.getPokemonName(i)
+        }
+        pokemons.push({
+          id: i,
+          name,
+          img: `${this.pokemonPictureUrl}${i}.png`
+        })
+      }
+      this.isLoading = false
+      return pokemons
     }
   }
 })
