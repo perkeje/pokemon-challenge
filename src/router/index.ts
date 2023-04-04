a920d2079ab89c119af7ced6444f33f031172b8a
jperic <juraj.peric@gmail.com>
2023-04-04 11:10:20 +0200

    route added

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PlayView from '../views/PlayView.vue'
+import PokedexView from '../views/PokedexView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: 'play',
       component: PlayView
     },
+    {
+      path: '/pokedex',
+      name: 'pokedex',
+      component: PokedexView
+    }
   ]
 })
 
