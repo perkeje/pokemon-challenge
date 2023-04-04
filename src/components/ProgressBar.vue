70fb166326b0759a048774575f1eb4a40e9579bb
jperic <juraj.peric@gmail.com>
2023-04-04 11:08:08 +0200

    progressbar added

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,52 @@
+<script setup lang='ts'>
+const props = defineProps({
+    progress: {
+        type: Number,
+        default: 0,
+    }
+})
+</script>
+
+<template>
+    <div class="outer">
+        <div class="inner" :style="{ 'width': `${props.progress}%` }">
+            <div class="inner-animate"></div>
+        </div>
+    </div>
+</template>
+
+<style>
+.outer {
+    width: 100%;
+    height: 100%;
+    background-color: var(--secondary-color);
+    border-radius: 10px;
+    border-color: var(--primary-color);
+    border-style: solid;
+}
+
+.inner {
+    width: 0%;
+    height: 100%;
+    border-radius: 10px;
+    border-style: hidden;
+}
+
+.inner-animate {
+    height: 100%;
+    background-color: #009DDC;
+    border-radius: 10px;
+    border-style: hidden;
+    animation: progress 1s ease-out 0.2s;
+    animation-fill-mode: backwards;
+}
+
+@keyframes progress {
+    from {
+        width: 0%;
+    }
+
+    to {
+        width: 100%;
+    }
+}</style>
